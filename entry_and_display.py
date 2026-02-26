62d2705901734a6f9efe0453a1ae36c2e34665a4
Neyder Ramirez <neider9725@gmail.com>
2026-02-26 07:23:07 -0500

    entry_and_display

---
+++ entry_and_display.py
@@ -0,0 +1,17 @@
+# Ejercicios de ingreso y muestra de datos
+
+# 1. Lee y muestra un número
+#   a. Pide al usuario que ingrese un número entero y muéstralo en pantalla.
+
+entero = int(input("Ingrese un número entero: "))
+print("El número ingresado es:", entero)
+
+
+# 2. Lee y muestra nombre y edad
+#   a. Pide al usuario su nombre y edad, luego muestra un mensaje como:
+#   "Hola [nombre], tienes [edad] años."
+
+nombre = input("Ingrese su nombre: ")
+edad = int(input("Ingrese su edad: "))
+print(f"Hola {nombre}, tienes {edad} años.")
+
